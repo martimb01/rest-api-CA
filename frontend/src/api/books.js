f79b84f8d28de7b55799267e1372db676b52877b
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-02-13 15:26:43 +0000

    Implemented both getBooks and updateBook

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -10,13 +10,34 @@ export const addNewBook = async (newTitle,newStart,newEnd) => {
         },
         body: JSON.stringify({
             title: newTitle,
-            start:newStart,
+            start: newStart,
             end: newEnd
         })
     })
+    const newBook = await response.json();
+    return newBook;
 }
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
+export const getBooks = async () => {
+    const response = await fetch(`${API_ENDPOINT}/books`);
+    const books = await response.json();
+
+    return books;
+};
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
-
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+    const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            newTitle,
+            newStart,
+            newEnd
+        })
+    })
+    return response.status;
+}
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
